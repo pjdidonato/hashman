ff23b1b63baee15d5f9eefe91bc88037ee54e4ba
pjdidonato <didona59@students.rowan.edu>
2021-04-19 19:06:34 -0400

    Added more loops

--- users.py
+++ users.py
@@ -1,26 +1,42 @@
-usernames = {}
+
+
+usernames = {'ID: 1' : 'PJ'}
+
+
 for guesses in range(0,3):
-    name = input("Please enter your name: ")
-    if name == "PJ":
+    user = input("Sign in or Create an Account: ")
+    
+    
+    if user == 'Sign in':
+        name = input("Please enter your name: ")
+        if name in usernames.values():
+            
+            break
+        elif name not in usernames.values():
+            guesses = guesses + 1
+            print("****Access denied*** " + str(guesses) + " guesses") 
+            print('Incorrect Name')
+            
+            
+    elif user == 'Create an Account':
+        name = input("Please enter your name: ")
+        usernames[name] = name
+        print("New Name Added: " + str(name))
         break
-    elif name != "PJ":
-        guesses = guesses + 1
-        print("****Access denied*** " + str(guesses) + " guesses") 
-        continue
+
+
 
 while True:
-    if name != "PJ":
-        print("Access denied you ran out of guesses")
-        value = input("Do you want to create an account? ")
-        if value == "No":
-            print("Good Bye")
-            break 
-        elif value == "Yes":
-            new = input("Enter your name: ")
-            usernames[name] = new
-            print("New Name Added: " + str(new))
-            break
-    elif name == "PJ":
-        print("Access Granted") 
+    if name not in usernames.values():
+        break 
+    if name in usernames.values():
+        print('Access granted')
         break
-
+    user = input('Do you want to sign in?: ')
+    if user == 'Yes':
+        name == input('Please enter your name: ')
+        if name in usernames.values():
+            print('Access Granted') 
+            break
+print("Access denied you ran out of guesses")        
+           
